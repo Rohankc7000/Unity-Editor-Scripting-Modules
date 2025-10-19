e5613e3445e633fa7535434e384e5ba8744f800f
Rohan Katuwal <rohankc700@gmail.com>
2025-10-19 17:50:20 +0545

    Has added the logic missing reference from gameobject scripts and console those objects

---
+++ Assets/Custom Missing References Detector/Editor/MissingReferenceDetector.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEditor;
+
+public class MissingReferenceDetector : EditorWindow
+{
+	[MenuItem("Window/Find Missing References")]
+	public static void ShowWindow()
+	{
+		EditorWindow window = GetWindow<MissingReferenceDetector>("Find Missing References");
+		window.maxSize = new Vector2(250, 100);
+		window.minSize = window.maxSize;
+
+	}
+
+	private void OnGUI()
+	{
+		EditorGUILayout.Space();
+		if (GUILayout.Button("Find Missing References", EditorStyles.boldLabel))
+		{
+			GameObject[] gameobjects = FindObjectsOfType<GameObject>();
+			foreach (GameObject go in gameobjects)
+			{
+				Component[] components = go.GetComponents<Component>();
+				foreach (Component component in components)
+				{
+					SerializedObject serialzedObject = new SerializedObject(component);
+					SerializedProperty serializedProperty = serialzedObject.GetIterator();
+					while (serializedProperty.NextVisible(true))
+					{
+						if (serializedProperty.propertyType == SerializedPropertyType.ObjectReference)
+						{
+							if (serializedProperty.objectReferenceValue == null)
+							{
+								Debug.Log($"<color=red><b>Missing reference: </b></color> {serializedProperty.displayName} on {go.name}", go);
+							}
+						}
+					}
+				}
+			}
+		}
+		Repaint();
+	}
+}
