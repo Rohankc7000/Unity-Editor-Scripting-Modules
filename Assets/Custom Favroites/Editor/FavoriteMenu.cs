fd82f0a43e010b372d9d85db5814825915f123ee
Rohan Katuwal <rohankc700@gmail.com>
2025-11-09 17:56:55 +0545

    Has added core logic of favorite system. Now the favorite items on the folder can be instantiated on the scene through Favorite Menu Item.

--- Assets/Custom Favroites/Editor/FavoriteMenu.cs
+++ Assets/Custom Favroites/Editor/FavoriteMenu.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -21,4 +22,67 @@ public class FavoriteMenu : EditorWindow
 		GameObject prefab = PrefabUtility.SaveAsPrefabAsset(gameObject, prefabPath);
 		favoritedObjects.Add(prefab);
 	}
+
+	private static void HandleFavoriteItemClicked(int index)
+	{
+		GameObject gameObject = favoritedObjects[index];
+		GameObject cloneOfGameObject = PrefabUtility.InstantiatePrefab(gameObject) as GameObject;
+		Camera currentSceneViewCamera = SceneView.lastActiveSceneView.camera;
+		Vector3 position = currentSceneViewCamera.ViewportToWorldPoint(new Vector3(.5f, .5f, 0f));
+		cloneOfGameObject.transform.position = position;
+		Selection.activeGameObject = cloneOfGameObject;
+		SceneView currentSceneView = SceneView.lastActiveSceneView;
+		currentSceneView.AlignViewToObject(cloneOfGameObject.transform);
+		EditorGUIUtility.PingObject(cloneOfGameObject);
+	}
+
+	[MenuItem("Favorites/Favorite Object 1", false, 50)]
+	public static void HandleFavoriteItem1Clicked()
+	{
+		HandleFavoriteItemClicked(0);
+	}
+
+	[MenuItem("Favorites/Favorite Object 1", true)]
+	public static bool HandleFavoriteItem1Option()
+	{
+		return favoritedObjects.Count >= 1;
+	}
+
+	[MenuItem("Favorites/Favorite Object 2", false, 50)]
+	public static void HandleFavoriteItem2Clicked()
+	{
+		HandleFavoriteItemClicked(1);
+	}
+
+	[MenuItem("Favorites/Favorite Object 2", true)]
+	public static bool HandleFavoriteItem2Option()
+	{
+		return favoritedObjects.Count >= 2;
+	}
+
+	[MenuItem("Favorites/Favorite Object 3", false, 50)]
+	public static void HandleFavoriteItem3Clicked()
+	{
+		HandleFavoriteItemClicked(2);
+	}
+
+	[MenuItem("Favorites/Favorite Object 3", true)]
+	public static bool HandleFavoriteItem3Option()
+	{
+		return favoritedObjects.Count >= 3;
+	}
+
+	[InitializeOnLoadMethod]
+	private static void Setup()
+	{
+		favoritedObjects.Clear();
+	}
+
+	[MenuItem("Favorites/Clear Favorites", false, 100)]
+	public static void ClearFavoriteMenu()
+	{
+		favoritedObjects.Clear();
+		EditorPrefs.DeleteAll();
+	}
+
 }
